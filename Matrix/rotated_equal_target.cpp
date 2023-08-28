11d47823871aeeb6ed4272642e6e26262f96ff73
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-08-28 14:35:23 +0530

    Create rotated_equal_target.cpp

---
+++ Matrix/rotated_equal_target.cpp
@@ -0,0 +1,25 @@
+// rotated matrix is equal to target matrix
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat){
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[i].size();j++){
+                if(i<j){
+                    swap(mat[i][j],mat[j][i]);
+                }
+            }
+        }
+        for(int i=0;i<mat.size();i++){
+            reverse(mat[i].begin(),mat[i].end());
+        }
+    }
+    
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        for(int i=0;i<4;i++){
+            rotate(mat);
+                if(mat==target)
+                    return true;
+            }
+        return false;
+    }
+};
